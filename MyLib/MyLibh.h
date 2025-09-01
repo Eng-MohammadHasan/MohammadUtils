97d9856537773818c386cc9a0a89ddf11d0c5994
Mohammad_Hasan <mohammad.hasan.prof@gmail.com>
2025-09-01 19:42:56 +0300

    Update MyLibh.h

--- MyLib/MyLibh.h
+++ MyLib/MyLibh.h
@@ -823,6 +823,60 @@ namespace MyLib
 			ClearFile(sourcePath);
 		}
 
+	void LoadDataFromFileToVector(string FileName, vector <string>& vFileContent)
+	{
+		fstream MyFile;
+		MyFile.open(FileName, ios::in);// read Mode
+	
+		if (MyFile.is_open())
+		{
+			string Line;
+	
+			while (getline(MyFile, Line))
+			{
+				vFileContent.push_back(Line);
+			}
+	
+			MyFile.close();
+		}
+}
+		
+		//void SaveVectorToFile(string FileName, const vector <string> & vFileContent)
+		void SaveVectorToFile(string FileName, vector <string> vFileContent)
+		{
+			fstream MyFile;
+			MyFile.open(FileName, ios::out);// Write Mode
+		
+			if (MyFile.is_open())
+			{
+				for (string& Line : vFileContent)
+				{
+					if (Line != "")
+					{
+						MyFile << Line << endl;
+					}
+				}
+		
+				MyFile.close();
+			}
+		}
+		
+		void DeleteRecordFromFile(string FileName, string Record)
+		{
+			vector <string> vFileContent;
+			LoadDataFromFileToVector(FileName, vFileContent);
+		
+			// don't forget the & (by ref), cuase we will edit on the vector's items
+			for (string& Line : vFileContent)
+			{
+				if (Line == Record)
+				{
+					Line = "";
+				}
+			}
+		
+			SaveVectorToFile(FileName, vFileContent);
+		}
 	}
 
 
